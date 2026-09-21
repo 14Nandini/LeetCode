586c6392e8ff06b162face1b65762ed61c28f7c9
Nandini Gedela <23A81A0581@sves.org.in>
2026-09-21 20:19:57 +0530

    Time: 414 ms (61.36%), Space: 0B (100%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select e1.name as Employee from Employee e1 
-inner join Employee e2 
-on e1.managerId = e2.id 
-where e1.salary > e2.salary;
+select e.name as Employee 
+from Employee e inner join Employee m
+on e.managerId = m.id
+where e.salary > m.salary;
